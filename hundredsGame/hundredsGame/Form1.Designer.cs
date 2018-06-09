5f60e6654b04d4ca406fc0cab1214ed727689887
Eliot <etroop719@gmail.com>
2018-06-09 10:44:50 -0700

    Got a ball!

--- hundredsGame/hundredsGame/Form1.Designer.cs
+++ hundredsGame/hundredsGame/Form1.Designer.cs
@@ -30,21 +30,41 @@
         {
             this.components = new System.ComponentModel.Container();
             this.gameRunner = new System.Windows.Forms.Timer(this.components);
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // gameRunner
             // 
             this.gameRunner.Enabled = true;
             this.gameRunner.Interval = 10;
+            this.gameRunner.Tick += new System.EventHandler(this.gameRunner_Tick);
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mainPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.mainPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(1884, 961);
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            this.mainPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mainPictureBox_MouseMove);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1884, 961);
+            this.Controls.Add(this.mainPictureBox);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -52,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Timer gameRunner;
+        private System.Windows.Forms.PictureBox mainPictureBox;
     }
 }
 
--- hundredsGame/hundredsGame/Form1.cs
+++ hundredsGame/hundredsGame/Form1.cs
@@ -5,21 +5,86 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Media;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace hundredsGame
 {
     public partial class Form1 : Form
     {
+        Bitmap canvas;
+        Graphics gfx;
+        List<bouncyball> balls = new List<bouncyball>();
+        bouncyball hundredBall1;
+        int cw;
+        int ch;
+        bool ball1move = true;
+        int mouseX;
+        int mouseY;
+        int ball1w = 20;
+        int ball1h = 20;
+        Rectangle rectangle = new Rectangle(0,0,2,2); 
+
         public Form1()
         {
             InitializeComponent();
+            canvas = new Bitmap(mainPictureBox.Width, mainPictureBox.Height);
+            gfx = Graphics.FromImage(canvas);
+            cw = ClientSize.Width;
+            ch = ClientSize.Height;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            bouncyball hundredBall1 = new bouncyball(Brushes.Black, 20, 20, 20, 20, 2, 2, true);
+             hundredBall1 = new bouncyball(Brushes.White, 20, 20, ball1w, ball1h, 2, 2, true);
+        }
+
+        private void gameRunner_Tick(object sender, EventArgs e)
+        {
+            
+            gfx.Clear(BackColor);
+            hundredBall1.Draw(gfx);
+            hundredBall1.Move(cw, ch, ball1move);
+            gfx.DrawRectangle(Pens.Red,rectangle);
+            mainPictureBox.Image = canvas;
+            if (rectangle.IntersectsWith(hundredBall1.HitBox))
+            {
+                hundredBall1.width += 1;
+                hundredBall1.height += 1;
+            }
+            if (hundredBall1.x < 0)
+            {
+                hundredBall1.x = 0;
+            }
+            if (hundredBall1.x + hundredBall1.width > ClientSize.Width)
+            {
+                hundredBall1.x -= 20; ;
+            }
+            if (hundredBall1.y < 0)
+            {
+                hundredBall1.y = 20;
+            }
+            if(hundredBall1.y + hundredBall1.height > ClientSize.Height)
+            {
+                hundredBall1.y -= 20 ;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+
+        }
+
+        private void mainPictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            mouseX = e.X;
+            mouseY = e.Y;
+            rectangle.X = e.X;
+            rectangle.Y = e.Y;
+
         }
     }
 }
